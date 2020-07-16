c669246172ca6f0925ae774496755843c67a116a
Jaime Rogstad <jaimerogstad@gmail.com>
2020-07-16 13:28:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,7 +25,7 @@ def won?(board)
   # iterate over WIN_COMBINATIONS (a nested array) and check if board has the same player token in each index of a winning combination
   WIN_COMBINATIONS.find do |win_combination|
     win_combination.all? do |index|
-      if board[index] == "X"
+      if board[index] == win_combination[index]
         true
       elsif board[index] == "O"
         true
