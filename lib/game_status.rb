04a301432f7d14783f93cff9dc3eaa6219552810
Jaime Rogstad <jaimerogstad@gmail.com>
2020-07-16 13:39:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,4 +1,5 @@
 require 'pry'
+#binding.pry
 
 # Helper Method
 def position_taken?(board, index)
@@ -24,8 +25,6 @@ def won?(board)
   # iterate over WIN_COMBINATIONS (a nested array) and check if board has the same player token in each index of a winning combination
   WIN_COMBINATIONS.find do |win_combination|
     board[win_combination].all? do |index|
-      #binding.pry
-
       if board[index] == "X"
         true
       elsif board[index] == "O"
